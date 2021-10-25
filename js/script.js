0a9f5160865b1a750c803e402801582cac2dedfa
lizfick <fickerliz@gmail.com>
2021-10-25 13:57:24 -0400

    update js
    added commas after each object, removed quotations from height and changed them to decimal numbers

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 const pokemonList = [
 { name: 'Weedle',
-  height: 1'00",
-  types: ['bug', 'poison'] }
+  height: 1,
+  types: ['bug', 'poison'] },
 
 { name: 'Jigglypuff',
-  height: 1' 08",
-  types: ['normal', 'fairy'] }
+  height: 1.08,
+  types: ['normal', 'fairy'] },
 
 { name: 'Dragonite',
-  height: 7' 03",
+  height: 7.03,
   types: ['dragon', 'flying'] }
 ];
