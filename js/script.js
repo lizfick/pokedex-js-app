5d944466670a0e620d721cf993e7939bd7dc0c8a
lizfick <fickerliz@gmail.com>
2021-10-24 21:26:36 -0400

    updated script.js with new variable
    created new variable assigned to blank array; added several objects with three keys - name(string), height(number), types(array of strings)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-alert('Hello world');
-const favoriteFood = 'Pasta';
-document.write(favoriteFood);
+const pokemonList = []
+
+{
+  name: 'Weedle',
+  height: 1'00",
+  types: ['bug', 'poison']
+}
+
+{
+  name: 'Jigglypuff',
+  height: 1' 08",
+  types: ['normal', 'fairy']
+}
+
+{
+  name: 'Dragonite',
+  height: 7' 03",
+  types: ['dragon', 'flying']
+}
+
+console.log(pokemonList);
