c484ab30d238883a25b96415acaf98d1117b9fd7
lizfick <fickerliz@gmail.com>
2021-10-25 09:37:25 -0400

    fixed array structure

--- js/script.js
+++ js/script.js
@@ -1,21 +1,15 @@
-const pokemonList = []
+const pokemonList = [
 
-{
-  name: 'Weedle',
+{ name: 'Weedle',
   height: 1'00",
-  types: ['bug', 'poison']
-}
+  types: ['bug', 'poison'] }
 
-{
-  name: 'Jigglypuff',
+{ name: 'Jigglypuff',
   height: 1' 08",
-  types: ['normal', 'fairy']
-}
+  types: ['normal', 'fairy'] }
 
-{
-  name: 'Dragonite',
+{ name: 'Dragonite',
   height: 7' 03",
-  types: ['dragon', 'flying']
-}
-
+  types: ['dragon', 'flying'] }
+]
 console.log(pokemonList);
