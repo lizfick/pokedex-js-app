6737f989fbf97ef97d3b858bfd1ea0cbe89379a7
lizfick <fickerliz@gmail.com>
2021-10-28 20:46:56 -0400

    Revert "added for loop"
    This reverts commit eb15147af27908586239391c49d8ee8cdd496954.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const pokemonList = [
 
 { name: 'Weedle',
-  height: 1,
+  height: 1.00,
   types: ['bug', 'poison'] },
 
 { name: 'Jigglypuff',
@@ -11,21 +11,5 @@ const pokemonList = [
 { name: 'Dragonite',
   height: 7.03,
   types: ['dragon', 'flying'] }
-
-{ name: 'Ivysaur',
-  height: 3.03,
-  types: ['grass', 'poison'] }
-
-{ name: 'Nidoqueen',
-  height: 4.03,
-  types: ['poison', 'ground'] }
 ]
 console.log(pokemonList);
-
-for (let i = 0; i < document.write; i++){
-  if (pokemonList[i].height < 2){
-    console.log(pokemonList[i].height + "Tiny!");
-  }else (pokemonList[i].height > 4.99){
-    console.log(pokemonList[i].height + "Huge!");
-  }
-}
