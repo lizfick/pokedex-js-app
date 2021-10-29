02906a0557c2ae2792ccd02eb36bfe4cefcc3fd3
lizfick <fickerliz@gmail.com>
2021-10-28 22:14:04 -0400

    update js with loop and conditonal
    added 2 more objects in array, added a for loop, added conditional within loop

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const pokemonList = [
 
 { name: 'Weedle',
-  height: 1.00,
+  height: 1,
   types: ['bug', 'poison'] },
 
 { name: 'Jigglypuff',
@@ -10,6 +10,25 @@ const pokemonList = [
 
 { name: 'Dragonite',
   height: 7.03,
-  types: ['dragon', 'flying'] }
+  types: ['dragon', 'flying'] },
+
+{ name: 'Ivysaur',
+  height: 3.03,
+  types: ['grass', 'poison'] },
+
+{ name: 'Nidoqueen',
+  height: 4.03,
+  types: ['poison', 'ground'] }
 ]
+
 console.log(pokemonList);
+
+//for loop used to lists the name and height of each pokemon in array
+//using <br> until I figure out how to make it a proper list
+for (let i = 0; i < pokemonList.length; i++){
+  document.write(pokemonList[i].name + " (height: " + pokemonList[i].height +" ft)" + '<br>');
+  //conditional within loop to determine tallest pokemon
+  if (pokemonList[i].height > 5) {
+    document.write(" - Wow, that's huge!" + '<br>');
+  }
+}
