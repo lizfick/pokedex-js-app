eb15147af27908586239391c49d8ee8cdd496954
lizfick <fickerliz@gmail.com>
2021-10-28 20:45:57 -0400

    added for loop
    added 2 new objects, added a for loop

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const pokemonList = [
 
 { name: 'Weedle',
-  height: 1.00,
+  height: 1,
   types: ['bug', 'poison'] },
 
 { name: 'Jigglypuff',
@@ -11,5 +11,21 @@ const pokemonList = [
 { name: 'Dragonite',
   height: 7.03,
   types: ['dragon', 'flying'] }
+
+{ name: 'Ivysaur',
+  height: 3.03,
+  types: ['grass', 'poison'] }
+
+{ name: 'Nidoqueen',
+  height: 4.03,
+  types: ['poison', 'ground'] }
 ]
 console.log(pokemonList);
+
+for (let i = 0; i < document.write; i++){
+  if (pokemonList[i].height < 2){
+    console.log(pokemonList[i].height + "Tiny!");
+  }else (pokemonList[i].height > 4.99){
+    console.log(pokemonList[i].height + "Huge!");
+  }
+}
